9bb86ca98ae318e52ac270dceb3fd6e6173827a7
liepsnele <liepsnele.tech@gmail.com>
2022-12-23 16:37:30 +0200

    Update

--- create-account.php
+++ create-account.php
@@ -1,10 +1,12 @@
 <?php
 
+session_start();
+
 $users = unserialize(file_get_contents(__DIR__ . '/users'));
 
 $ibanValue = 'LT' . ' ' . rand(0, 9) . rand(0, 9) . ' ' . '0014' . ' ' . '7' . rand(0, 9) . rand(0, 9) . rand(0, 9) . ' ' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)  . ' ' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
   if (preg_match('/[a-ząčęėįšųū\s]{4,}/i', $_POST['name'])) {
     $name = $_POST['name'];
   } else {
@@ -17,6 +19,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
   if (preg_match('/^[1-6]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])\d{4}$/', $_POST['personal-number'])) {
+    foreach ($users as $user) {
+      if ($user['personal-number'] === $_POST['personal-number']) {;
+        header("http://localhost/smartmoney/create-account.php");
+        die;
+      }
+    }
     $personalNumber = $_POST['personal-number'];
   } else {
     echo 'Neteisingas asmens kodas.';
@@ -39,6 +47,7 @@ require __DIR__ . './inc/header.php';
 ?>
 
 <main class="main-inner container">
+  <h1 class="main-title">Sukurti sąskaitą</h1>
   <div class="row w-100 justify-content-center">
 
     <div class="col- 10 col-lg-5">
@@ -64,7 +73,7 @@ require __DIR__ . './inc/header.php';
             readonly>
         </div>
 
-        <button type="submit" class="btn btn-main btn-green">Sukurti</button>
+        <button type="submit" name="submit" class="btn btn-main btn-green">Sukurti</button>
       </form>
 
       <?php if (isset($error)) : ?>
